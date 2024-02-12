2f9ee643b18863d8ce62cb51ed5460e8fc061456
Sushil <sushilwaghmare2048@gmail.com>
2024-02-12 18:14:39 +0530

    API call for longitude and latitude

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+import gpsd
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+
+class Location(BaseModel):
+    latitude: float
+    longitude: float
 
 @app.get("/")
 async def read_index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/send-location")
-async def send_location(latitude: float, longitude: float):
-    print(f"Latitude: {latitude}, Longitude: {longitude}")
-    return {"latitude": latitude, "longitude": longitude}
+async def send_location(location: Location):
+    print(f"Latitude: {location.latitude}, Longitude: {location.longitude}")
+    
+
+    return {"latitude": location.latitude, "longitude": location.longitude}
 
 if __name__ == "__main__":
     import uvicorn
