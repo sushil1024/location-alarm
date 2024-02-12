5567467012a538736811c4b3188b8253d089825b
Sushil <sushilwaghmare2048@gmail.com>
2024-02-12 17:38:46 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,31 +1,18 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
-import gpsd
 
 app = FastAPI()
-
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
-def index(request: Request):
-    # return templates.TemplateResponse("index.html", {"request": request})
+async def read_index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
-    loct = None
+@app.post("/send-location")
+async def send_location(latitude: float, longitude: float):
+    print(f"Latitude: {latitude}, Longitude: {longitude}")
+    return {"latitude": latitude, "longitude": longitude}
 
-    try:
-        gpsd.connect()
-        loct = gpsd.get_current()
-
-        print(loct.position())
-
-    except Exception as e:
-        print(f"Error: {e}")
-
-    if loct is None:
-        return {"location": "No location data available"}
-    else:
-        return {"location": f"{loct.position()}"}
-    
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
