eb67fa3a6701c505c2aa06f7747299e860f5a9c8
Sushil <sushilwaghmare2048@gmail.com>
2024-02-13 01:30:00 +0530

    add style and debug config

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
-import gpsd
+import googlemaps
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 class Location(BaseModel):
     latitude: float
@@ -15,12 +17,25 @@ async def read_index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/send-location")
-async def send_location(location: Location):
-    print(f"Latitude: {location.latitude}, Longitude: {location.longitude}")
-    
+async def send_location(location: Location, request: Request):
+    latitude = location.latitude
+    longitude = location.longitude
+    print(f"Latitude, Longitude: {location.latitude}, {location.longitude}")
 
-    return {"latitude": location.latitude, "longitude": location.longitude}
+    f = open('api_key.txt', 'r')
+    api_key = f.read()
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    gmaps = googlemaps.Client(key=api_key)
+    reverse_geocode_result = gmaps.reverse_geocode((19.013944, 72.827631))
+
+    current_address = reverse_geocode_result[0]['formatted_address']
+
+    print(current_address)
+
+    if 'grant road' in str(current_address).lower():
+        print("We're in grant road")
+    else:
+        print("We're not anywhere")
+
+    return templates.TemplateResponse("index.html", {"request": request, "latitude": latitude, "longitude": longitude})
+
